7c9923ddc050b4f5647cac8942a206c931947d4c
Nikita <nikitakononchik@gmail.com>
2023-10-01 23:59:28 +0300

    Edit errors practIce

--- programming-basics-1/02-errors/refactoring/DrawingProgram_Tests.cs
+++ programming-basics-1/02-errors/refactoring/DrawingProgram_Tests.cs
@@ -3,37 +3,30 @@ using System.Drawing;
 using System.Reflection;
 using NUnit.Framework;
 
-namespace RefactorMe
-{
+namespace RefactorMe {
     [TestFixture]
-    public class DrawingProgram_Tests
-    {
+    public class DrawingProgram_Tests {
         [Test]
-        public void DrawExpectedImage()
-        {
+        public void DrawExpectedImage() {
             const int width = 800;
             const int height = 600;
             var actual = new Bitmap(width, height);
             ImpossibleSquare.Draw(width, height, 0, Graphics.FromImage(actual));
-			//actual.Save(TestContext.CurrentContext.TestDirectory + "/expected-image.bmp");
+            //actual.Save(TestContext.CurrentContext.TestDirectory + "/expected-image.bmp");
 
-			using (var stream = Assembly.GetExecutingAssembly()
-				       .GetManifestResourceStream(typeof(DrawingProgram_Tests), "expected-image.bmp"))
-			{
-				Assume.That(stream, Is.Not.Null);
-				var expectedImage = (Bitmap)Image.FromStream(stream);
-				AssertImageEquals(expectedImage, actual);
-			}
-		}
+            using (var stream = Assembly.GetExecutingAssembly()
+                       .GetManifestResourceStream(typeof(DrawingProgram_Tests), "expected-image.bmp")) {
+                Assume.That(stream, Is.Not.Null);
+                var expectedImage = (Bitmap)Image.FromStream(stream);
+                AssertImageEquals(expectedImage, actual);
+            }
+        }
 
-        public void AssertImageEquals(Bitmap expected, Bitmap actual)
-        {
+        public void AssertImageEquals(Bitmap expected, Bitmap actual) {
             var diffCount = 0.0;
 
-            for (var x = 0; x < expected.Width; x++)
-            {
-                for (var y = 0; y < expected.Height; y++)
-                {
+            for (var x = 0; x < expected.Width; x++) {
+                for (var y = 0; y < expected.Height; y++) {
                     Color expectedPixel = expected.GetPixel(x, y);
                     Color actualPixel = actual.GetPixel(x, y);
                     if (Math.Abs(expectedPixel.GetBrightness() - actualPixel.GetBrightness()) > 0.1)
