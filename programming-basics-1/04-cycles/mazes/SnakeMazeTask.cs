c905cd79c90ea02fc641bdcea493e6c36e10ca8c
Nikita <nikitakononchik@gmail.com>
2023-10-02 11:26:56 +0300

    Refact cycles practice

--- programming-basics-1/04-cycles/mazes/SnakeMazeTask.cs
+++ programming-basics-1/04-cycles/mazes/SnakeMazeTask.cs
@@ -1,30 +1,24 @@
 ﻿using System.Drawing;
 
-namespace Mazes
-{
-    public static class SnakeMazeTask
-    {
-        public static void MoveOut(Robot robot, int width, int heigth)
-        {
-            while (true)
-            {
+namespace Mazes {
+    public static class SnakeMazeTask {
+        public static void MoveOut(Robot robot, int width, int heigth) {
+            while (true) {
                 MoveHorizontal(robot, width);
                 if (robot.Finished) break;
-                MoveInDirection(robot, Direction.Down, 2);
+                robot.MoveTo(Direction.Down, 2);
                 if (robot.Finished) break;
             }
         }
 
-        static void MoveHorizontal(Robot robot, int width)
-        {
-            if (robot.X < (width - 2) / 2)
-                MoveInDirection(robot, Direction.Right, width - 3);
-            else
-                MoveInDirection(robot, Direction.Left, width - 3);
+        static void MoveHorizontal(Robot robot, int width) {
+            if (robot.X < (width - 2) / 2) robot.MoveTo(Direction.Right, width - 3);
+            else robot.MoveTo(Direction.Left, width - 3);
         }
+    }
 
-        static void MoveInDirection(Robot robot, Direction direction, int steps)
-        {
+    public static class RobotExtensions {
+        public static void MoveTo(this Robot robot, Direction direction, int steps) {
             for (int i = 0; i < steps; i++)
                 robot.MoveTo(direction);
         }
