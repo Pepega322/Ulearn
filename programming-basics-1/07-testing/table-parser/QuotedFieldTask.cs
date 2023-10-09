83d8751a6e467151a2bff8763cb4f0e2a3b1a423
Nikita <nikitakononchik@gmail.com>
2023-10-09 15:45:27 +0300

    Refact testing practice

--- programming-basics-1/07-testing/table-parser/QuotedFieldTask.cs
+++ programming-basics-1/07-testing/table-parser/QuotedFieldTask.cs
@@ -1,11 +1,9 @@
 using System.Text;
 using NUnit.Framework;
 
-namespace TableParser
-{
+namespace TableParser {
     [TestFixture]
-    public class QuotedFieldTaskTests
-    {
+    public class QuotedFieldTaskTests {
         [TestCase("''", 0, "", 2)]
         [TestCase("\"\"", 0, "", 2)]
         [TestCase("'a'", 0, "a", 3)]
@@ -16,68 +14,42 @@ namespace TableParser
         [TestCase("'\"1\" \"2\" \"3\"'", 0, "\"1\" \"2\" \"3\"", 13)]
         [TestCase("\"QF \\\"\"", 0, "QF \"", 7)]
 
-        public void Test(string line, int startIndex, string expectedValue, int expectedLength)
-        {
+        public void Test(string line, string expectedValue, int startIndex, int expectedLength) {
             var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
             Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
         }
     }
 
-    class QuotedFieldTask
-    {
-        public static Token ReadQuotedField(string line, int startIndex)
-        {
-            var countOpenQuote = IsEndQuoteExist(line, startIndex) ? 2 : 1;
-            var countSlash = CountSlash(line, startIndex);
-            var value = GetQuotedTokenValue(line, startIndex);
-            var length = value.Length + countSlash + countOpenQuote;
-
-            return new Token(value, startIndex, length);
-        }
-
-        private static string GetQuotedTokenValue(string line, int startIndex)
-        {
-            var valueBuilder = new StringBuilder();
-            for (var i = startIndex + 1; i < line.Length; i++)
-            {
-                if (line[i] == line[startIndex]) break;
-                if (line[i] == '\\')
-                {
-                    valueBuilder.Append(line[i + 1]);
-                    i++;
-                    continue;
-                }
-                valueBuilder.Append(line[i]);
-            }
-
-            return valueBuilder.ToString();
-        }
-
-        private static int CountSlash(string line, int startIndex)
-        {
-            var countSlash = 0;
-            for (var i = startIndex + 1; i < line.Length; i++)
-            {
-                if (line[i] == line[startIndex] && line[i - 1] != '\\') break;
-                if (line[i] == '\\') countSlash++;
-            }
-
-            return countSlash;
-        }
-
-        private static bool IsEndQuoteExist(string line, int startIndex)
-        {
-            var isEndQuoteExist = false;
-            for (var i = startIndex + 1; i < line.Length; i++)
-            {
-                if (line[i] == line[startIndex] && line[i - 1] != '\\')
-                {
-                    isEndQuoteExist = true;
+    public class QuotedFieldTask {
+        public static Token ReadQuotedField(string line, int startIndex) {
+            var quoteCount = 1;
+            var slashCount = 0;
+            for (var i = startIndex + 1; i < line.Length; i++) {
+                if (line[i] == '\\') slashCount++;
+                if (line[i] == line[startIndex] && line[i - 1] != '\\') {
+                    quoteCount = 2;
                     break;
                 }
             }
 
-            return isEndQuoteExist;
+            var value = GetQuotedTokenValue(line, startIndex);
+            var length = value.Length + slashCount + quoteCount;
+            return new Token(value, startIndex, length);
+        }
+
+        private static string GetQuotedTokenValue(string line, int startIndex) {
+            var b = new StringBuilder();
+            var i = startIndex + 1;
+            while (i < line.Length && line[i] != line[startIndex]) {
+                if (line[i] == '\\') {
+                    b.Append(line[i + 1]);
+                    i += 2;
+                    continue;
+                }
+                b.Append(line[i]);
+                i++;
+            }
+            return b.ToString();
         }
     }
 }
