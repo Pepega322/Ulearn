b0bd3e6fb25089c6518e7ad734b9c4caaaa919a4
Nikita <nikitakononchik@gmail.com>
2023-10-25 23:21:16 +0300

    Refact manipulator practice (download newer version from site, than refact)

--- programming-basics-1/11-practice/manipulator/TriangleTask.cs
+++ programming-basics-1/11-practice/manipulator/TriangleTask.cs
@@ -1,33 +1,26 @@
 using System;
 using NUnit.Framework;
 
-namespace Manipulation
-{
-    public class TriangleTask
-    {
-        public static double GetABAngle(double a, double b, double c)
-        {
-            if (a > 0 && b > 0 && c >= 0)
-                return Math.Acos((a * a + b * b - c * c) / (2 * a * b));
-            else
-                return double.NaN;
-        }
-    }
+namespace Manipulation;
 
-    [TestFixture]
-    public class TriangleTask_Tests
-    {
-        [TestCase(0.198177329356865, 0.715455999465406, 0.793295098838068, 1.8500559680422)]
-        [TestCase(3, 4, 5, Math.PI / 2)]
-        [TestCase(1, 1, 1, Math.PI / 3)]
-        [TestCase(1, 1, 0, 0)]
-        [TestCase(2.001, 1, 1, double.NaN)]
-        [TestCase(0, 1, 1, double.NaN)]
-        [TestCase(1, -0.1, 1, double.NaN)]
-        public void TestGetABAngle(double a, double b, double c, double expectedAngle)
-        {
-            var actualAngle = TriangleTask.GetABAngle(a, b, c);
-            Assert.AreEqual(expectedAngle, actualAngle, 1e-8);
-        }
+public class TriangleTask {
+    public static double GetABAngle(double a, double b, double c)
+        => (a < 0 || b < 0 || c < 0) ? double.NaN : Math.Acos((a * a + b * b - c * c) / (2 * a * b));
+}
+
+[TestFixture]
+public class TriangleTask_Tests {
+    [TestCase(3, 4, 5, Math.PI / 2)]
+    [TestCase(1, 1, 1, Math.PI / 3)]
+    [TestCase(0.198177329356865, 0.715455999465406, 0.793295098838068, 1.8500559680422)]
+    [TestCase(3, 4, 5, Math.PI / 2)]
+    [TestCase(1, 1, 1, Math.PI / 3)]
+    [TestCase(1, 1, 0, 0)]
+    [TestCase(2.001, 1, 1, double.NaN)]
+    [TestCase(0, 1, 1, double.NaN)]
+    [TestCase(1, -0.1, 1, double.NaN)]
+    public void TestGetABAngle(double a, double b, double c, double expectedAngle) {
+        var actualAngle = TriangleTask.GetABAngle(a, b, c);
+        Assert.AreEqual(expectedAngle, actualAngle, 1e-8);
     }
 }
